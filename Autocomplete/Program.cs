389c7d84495d33f616fd4466ca12e7c46d58d148
Jack Dryer <jackjbld@gmail.com>
2023-03-09 14:42:21 +0000

    Added update on restart

--- Autocomplete/Program.cs
+++ Autocomplete/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
 using System.Windows.Forms;
+using Serilog;
 namespace Autocomplete
 {
     static class Program
@@ -12,7 +15,43 @@ namespace Autocomplete
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainProgram());
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            currentDomain.UnhandledException += new UnhandledExceptionEventHandler(MyHandler);
+            Application.ThreadException += new System.Threading.ThreadExceptionEventHandler(Application_ThreadException);
+            try
+            {
+                Application.Run(new MainProgram());
+            }
+            catch (Exception e)
+            {
+                LogException(e,"In try catch");
+                Application.Restart();
+            }
+        }
+        private static void LogException(Exception e,string message)
+        {
+            Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.File("autocomplete.log", rollingInterval: RollingInterval.Infinite)
+    .CreateLogger();
+            Log.Error(e, message);
+            Log.CloseAndFlush();
+        }
+        static void MyHandler(object sender, UnhandledExceptionEventArgs args)
+        {
+            Exception e = (Exception)args.ExceptionObject;
+            LogException(e, "In thread");
+            RestartApplication();
+        }
+        static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+            LogException(e.Exception, "in Application Thread");
+            RestartApplication();
+        }
+        private static void RestartApplication()
+        {
+            Process.Start(Application.ExecutablePath);
+            Environment.Exit(0);
         }
     }
 }
